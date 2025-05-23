96af432a1d95b6c739b440e649fc729ca4358ad7
isaac <isaac.j.mayo@gmail.com>
2025-05-22 21:27:38 -0700

    working on java game

--- src/Main.java
+++ src/Main.java
@@ -12,33 +12,40 @@ public class Main {
 }
 class Window extends JFrame {
     //create the base components
-    JPanel p1 = new JPanel();
-    JFrame f1 = new JFrame();
-    JButton b1 = new JButton("fml");
+    JPanel mainPanel = new JPanel();
+    JFrame frame = new JFrame();
+    JButton paddleP = new JButton();
+    JButton paddleE = new JButton();
+    JButton ball = new JButton();
     JTextField keyboard = new JTextField();
+
     public void constructGame() {
         //properly create the check for key presses
         keyboard.addKeyListener(new Keycheck(this));
 
-        // Size the button properly
-        b1.setBounds(0, 0, 20, 20);
+        // Size the players paddle properly
+        paddleP.setBounds(0, 0, 20, 70);
+        paddleP.setIcon(new ImageIcon());
 
         // create JPanel and add the button (and keyboard)
-        p1.setSize(new Dimension(400, 300));
-        p1.add(b1);
-        p1.add(keyboard);
-        p1.setLayout(null);
+        mainPanel.setSize(new Dimension(400, 300));
+        mainPanel.add(paddleP);
+        mainPanel.add(keyboard);
+        mainPanel.setLayout(null);
 
         // Make the JFrame work
-        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f1.setSize(400, 300);
-        f1.setResizable(false);
-        f1.setLayout(null);
-        f1.add(p1);
-        f1.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.add(mainPanel);
+        frame.setVisible(true);
     }
     public void updateButtonPosition(int x, int y, JButton b) {
         b.setLocation(x, y);
+    }
+    public void beginGame() {
+
     }
 }
 //class that aids the JTextField in searching for keys
