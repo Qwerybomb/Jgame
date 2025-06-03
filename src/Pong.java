a33b4936d6af1e876d5d39f2d8e244f8186711c4
isaac <isaac.j.mayo@gmail.com>
2025-06-03 15:07:39 -0700

    Both paddles can now move properly.

--- src/Pong.java
+++ src/Pong.java
@@ -7,10 +7,10 @@ import java.nio.file.*;
 import java.nio.file.Paths;
 import java.util.concurrent.atomic.*;
 
-public class Main {
+public class Pong {
 
     public static void main(String[] args) throws URISyntaxException {
-        Path baseParent = Paths.get(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
+        Path baseParent = Paths.get(Pong.class.getProtectionDomain().getCodeSource().getLocation().toURI());
         Path assetLocation = baseParent.resolve("Assets");
         game gameObj = new game();
         gameObj.constructGame(assetLocation);
@@ -23,17 +23,14 @@ interface baseLogic { // contains the base swing components to be implemented an
     JFrame mainFrame = new JFrame();
     JLabel paddleP1 = new JLabel();
     JLabel paddleP2 = new JLabel();
-    JButton ball = new JButton();
+    JLabel ball = new JLabel();
     JTextField keyboard = new JTextField();
     JLabel coolBg = new JLabel();
 
     // create global variables (atomic may be needed due to the nature of interfaces)
     static int speed = 4;
-    AtomicBoolean moveToggleP1 = new AtomicBoolean(false);
-    AtomicBoolean moveToggleP2 = new AtomicBoolean(false);
     AtomicInteger upOrDownMPp1 = new AtomicInteger(0);
     AtomicInteger upOrDownMPp2 = new AtomicInteger(0);
-
     // allow a Component Label to be repositioned
     public static void updateButtonPosition(int x, int y, JLabel b) {
         b.setLocation(x, y);
@@ -45,18 +42,24 @@ class game implements baseLogic {
     public void constructGame(Path imageFolder) {
         // Format the paddles
         ImageIcon paddleTexture = new ImageIcon(String.valueOf(imageFolder.resolve("paddle.png")));
-        paddleP1.setBounds(0, 0, 20, 70);
+        paddleP1.setBounds(0, 50, 20, 70);
         paddleP1.setBorder(null);
-        paddleP2.setBounds(365,0,20,70);
+        paddleP2.setBounds(365,50,20,70);
         paddleP2.setBorder(null);
         paddleP1.setIcon(paddleTexture);
         paddleP2.setIcon(paddleTexture);
 
         // make cool background
         ImageIcon bgImg = new ImageIcon(String.valueOf(imageFolder.resolve("grid.png")));
-        coolBg.setBounds(25,0,bgImg.getIconWidth(),bgImg.getIconHeight());
+        coolBg.setBounds(0,0,bgImg.getIconWidth(),bgImg.getIconHeight());
         coolBg.setBorder(null);
         coolBg.setIcon(bgImg);
+
+        // make the ball for the gmae :3
+        ImageIcon ballIcon = new ImageIcon(String.valueOf(imageFolder.resolve("ball.png")));
+        ball.setBounds(100,100,ballIcon.getIconWidth(),ballIcon.getIconHeight());
+        ball.setIcon(ballIcon);
+        ball.setBorder(null);
 
         // create JPanel and add the button (and keyboard)
         mainPanel.setSize(new Dimension(400, 300));
@@ -64,54 +67,64 @@ class game implements baseLogic {
         mainPanel.add(paddleP1);
         mainPanel.add(paddleP2);
         mainPanel.add(keyboard);
+        mainPanel.add(ball);
         mainPanel.setLayout(null);
         mainPanel.setBackground(Color.black);
-        
+
         // Make the JFrame work
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.setSize(400, 300);
+        mainFrame.setSize(new Dimension(400, 300));
         mainFrame.setResizable(false);
         mainFrame.setLayout(null);
         mainFrame.add(mainPanel);
         mainFrame.setVisible(true);
-        mainFrame.addKeyListener(new Keycheck());
+        mainFrame.addKeyListener(new keyCheck());
         mainFrame.requestFocusInWindow();
 
         //properly setting up the listener
-        keyboard.addKeyListener(new Keycheck());
+        keyboard.addKeyListener(new keyCheck());
         keyboard.requestFocusInWindow();
     }
     public void beginGame() {
         while (true) {
-            // paddle Movement Logic
-        if (moveToggleP1.get()) {
+        // paddle Movement Logic
             baseLogic.paddleP1.setLocation(0, baseLogic.paddleP1.getY() + speed * upOrDownMPp1.get());
+            baseLogic.paddleP2.setLocation(365, baseLogic.paddleP2.getY() + speed * upOrDownMPp2.get());
+            if (paddleP1.getY() < 0) paddleP1.setLocation(0, 0);
+            if (paddleP1.getY() > 200) paddleP1.setLocation(0, 200);
+            if (paddleP2.getY() < 0) paddleP2.setLocation(365, 0);
+            if (paddleP2.getY() > 200) paddleP2.setLocation(365, 200);
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        }
-
-
-
+            // Continue Logic path here
 
         }
+
     }
 }
 //class that aids the JTextField in searching for keys
- class Keycheck extends KeyAdapter implements baseLogic {
+ class keyCheck extends KeyAdapter implements baseLogic {
     char ch = '0';
-     @Override
-     public void keyPressed(KeyEvent event) {
+
+    @Override
+    public void keyPressed(KeyEvent event) {
         // code to run when Key press event happens goes here
         ch = event.getKeyChar();
-        moveToggleP1.set(true);
-        if (ch == 'w') upOrDownMPp1.set(1);
-        if (ch == 's') upOrDownMPp1.set(-1);
+        if (ch == 's') upOrDownMPp1.set(1);
+        if (ch == 'w') upOrDownMPp1.set(-1);
+        if (ch == 'k') upOrDownMPp2.set(1);
+        if (ch == 'i') upOrDownMPp2.set(-1);
     }
-     @Override
-     public void keyReleased(KeyEvent event) {
-         moveToggleP1.set(false);
- }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+        ch = event.getKeyChar();
+        if (ch == 's') upOrDownMPp1.set(0);
+        if (ch == 'w') upOrDownMPp1.set(0);
+        if (ch == 'k') upOrDownMPp2.set(0);
+        if (ch == 'i') upOrDownMPp2.set(0);
+    }
 }
